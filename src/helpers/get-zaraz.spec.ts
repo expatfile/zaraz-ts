1edbdd2e3b67099b40c36bf217b4d407df78b01f
Zino Hofmann <zino@hofmann.amsterdam>
2024-01-17 08:46:44 +0000

    feat: ✨ queue zaraz method calls until initialised

--- src/helpers/get-zaraz.spec.ts
+++ src/helpers/get-zaraz.spec.ts
@@ -13,12 +13,20 @@ declare global {
 
 let windowObj: Window & typeof globalThis;
 
+const logSpy = jest.spyOn(console, 'log');
+
 beforeAll(() => {
   windowObj = window;
+
+  logSpy.mockImplementation();
 });
 
 afterAll(() => {
-  window = windowObj;
+  Object.defineProperty(global, 'window', {
+    value: windowObj,
+  });
+
+  logSpy.mockRestore();
 });
 
 describe('getZaraz()', () => {
@@ -36,11 +44,57 @@ describe('getZaraz()', () => {
     });
   });
 
-  it('should throw when zaraz does not exist on the window', () => {
+  it('should queue events when zaraz is not defined', () => {
     window.zaraz = undefined;
 
+    // getZaraz() returns a queue.
+    getZaraz().track('page_view', {
+      page_location: 'https://example.com',
+      page_path: '/',
+      page_title: 'Home',
+    });
+
+    expect(logSpy).toHaveBeenCalledWith(
+      `Zaraz Web API is not initialized. Queueing events...`,
+    );
+
+    window.zaraz = {
+      track: trackMock,
+      set: setMock,
+      ecommerce: ecommerceMock,
+    };
+
+    // Triggers the flush.
+    getZaraz().track('button clicked', { userId: 'ABC-123', value: 200 });
+
+    expect(logSpy).toHaveBeenCalledWith(
+      `Zaraz Web API is initialized. Flushing queue...`,
+    );
+
+    // Calls the queued events.
+    expect(trackMock).toHaveBeenCalledWith('page_view', {
+      page_location: 'https://example.com',
+      page_path: '/',
+      page_title: 'Home',
+    });
+
+    // Calls the actual event.
+    expect(trackMock).toHaveBeenCalledWith('button clicked', {
+      userId: 'ABC-123',
+      value: 200,
+    });
+
+    expect(logSpy).toHaveBeenCalledWith(`Zaraz Web API queue flushed.`);
+  });
+
+  it('should throw when window is not defined', () => {
+    // Set window to undefined.
+    Object.defineProperty(global, 'window', {
+      value: undefined,
+    });
+
     expect(() => getZaraz()).toThrow(
-      `Cannot use Zaraz Web API, because window.zaraz is not defined.`,
+      `Cannot use Zaraz Web API, because window is not defined.`,
     );
   });
 });
