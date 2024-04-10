43d5aaf85ebaa1650732509444f8b7938e7509db
Ural <uralkrc@gmail.com>
2024-04-10 11:10:40 +0200

    feat(add-consent): remove error handling

--- src/consent.ts
+++ src/consent.ts
@@ -2,9 +2,5 @@ import { getZaraz } from './helpers/get-zaraz';
 
 export function setConsent(key: string, value: boolean): void {
   const zaraz = getZaraz();
-  if (zaraz && zaraz.consent) {
-    zaraz.consent[key] = value;
-  } else {
-    console.error('Zaraz consent API is not available');
-  }
+  zaraz.consent[key] = value;
 }
