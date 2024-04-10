efc14ed9fefb156af1e79a6307953d1a31e74c0b
Ural <uralkrc@gmail.com>
2024-04-10 16:40:40 +0200

    feat(add-consent): test for index.ts

---
+++ src/consent/index.spec.ts
@@ -0,0 +1,16 @@
+import * as consentModule from './index';
+
+describe('consent', () => {
+  it('should have the expected methods', () => {
+    expect(consentModule.consent).toEqual({
+      get: expect.any(Function),
+      set: expect.any(Function),
+      getAll: expect.any(Function),
+      setAll: expect.any(Function),
+      getAllCheckboxes: expect.any(Function),
+      setCheckboxes: expect.any(Function),
+      setAllCheckboxes: expect.any(Function),
+      sendQueuedEvents: expect.any(Function),
+    });
+  });
+});
